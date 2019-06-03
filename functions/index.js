e2d5ecb7c1032195ef608119d6e53d7c46dc4d7c
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-03 14:48:42 +0900

    Add a sign up functionaity and connect it with firebase authentication

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,10 @@ admin.initializeApp();
 const express = require('express');
 const app = express();
 
+const firebase = require('firebase')
+const config = require('./utils/config')
+firebase.initializeApp(config);
+
 app.get('/words', (req, res) => {
   admin.firestore()
     .collection('words')
@@ -50,5 +54,24 @@ app.post('/word', (req, res) => {
       console.err(err);
     });
 });
+
+// Sign Up route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    name: req.body.name
+  };
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res.status(201).json({ message: `User ${data.user.uid} signed up successfully`})
+    })
+    .catch(err => {
+      console.err(err);
+      return res.status(500).json({ error: err.code });
+    })
+})
 
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
