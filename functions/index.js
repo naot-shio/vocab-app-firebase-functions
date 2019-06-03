dc4f7ff3d1e189a9fbdc642b8a0c271304b1285f
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-03 13:19:28 +0900

    Add a create functionality

--- functions/index.js
+++ functions/index.js
@@ -20,4 +20,26 @@ app.get('/words', (req, res) => {
     .catch(err => console.error(err));
 });
 
+app.post('/word', (req, res) => {
+  const newWord = {
+    userName: req.body.userName,
+    english: req.body.english,
+    japanese: req.body.japanese,
+    sentence: req.body.sentence,
+    translation: req.body.translation,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  
+  admin.firestore()
+    .collection('words')
+    .add(newWord)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully`});
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Something went wrong'});
+      console.err(err);
+    });
+});
+
 exports.api = functions.https.onRequest(app);
