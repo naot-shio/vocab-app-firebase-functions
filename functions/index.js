13e8a780d8aa1c3394446b0612930a38631040c0
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-04 10:43:51 +0900

    Add a functionality to get user details

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const FBAuth = require('./utils/fbAuth');
 
 // word routes
 const { getAllWords, createWord } = require('./handlers/words');
-const { signUp, login, imageUploader } = require('./handlers/users');
+const { signUp, login, imageUploader, getOwnDetails } = require('./handlers/users');
 
 app.get('/words', getAllWords);
 app.post('/word', FBAuth, createWord);
@@ -16,5 +16,6 @@ app.post('/word', FBAuth, createWord);
 app.post('/signup', signUp);
 app.post('/login', login);
 app.post('/user/image', FBAuth, imageUploader);
+app.get('/user', FBAuth, getOwnDetails);
 
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
