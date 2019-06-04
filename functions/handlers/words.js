5dd20a94a1f5c68f35fea6c34fdd5f1c79332fde
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-04 13:06:37 +0900

    Refactor like/unlike code

--- functions/handlers/words.js
+++ functions/handlers/words.js
@@ -1,4 +1,5 @@
 const { db } = require('../utils/admin')
+const { findLikeDocument, findWordDocument, isDocumentFound } = require('../utils/dbDocument')
 
 exports.getAllWords = (req, res) => {
   db
@@ -50,77 +51,70 @@ exports.createWord = (req, res) => {
 }
 
 exports.likeWord = (req, res) => {
-  const likeDocument = db
-    .collection('likes')
-    .where('userName', '==', req.user.name)
-    .where('wordId', '==', req.params.wordId)
-    .limit(1);
+  const likeDocument = findLikeDocument(req);
+  const wordDocument = findWordDocument(req);
+  let wordData = {};
 
-    const wordDocument = db.doc(`/words/${req.params.wordId}`)
-
-    let wordData = {};
-
-    wordDocument
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          wordData = doc.data();
-          wordData.wordId = doc.id;
-          return likeDocument.get()
-        }
-        return res.status(404).json({ error: 'Not Found' })
-      })
-      .then(data => {
-        if (data.empty) return db
-          .collection('likes')
-          .add({
-            wordId: req.params.wordId,
-            userName: req.user.name
-          })
-          .then(() => {
-            wordData.likeCount++;
-            return wordDocument.update({ likeCount: wordData.likeCount })
-          })
-          .then(() => res.json(wordData))
-          .catch(err => res.status(500).json({ error: err.code }));
-        return res.status(400).json({ error: 'Already liked'});
-      })
-      .catch(err => res.status(500).json({ error: err.code }));
+  wordDocument
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        wordData = doc.data();
+        wordData.wordId = doc.id;
+        return likeDocument.get();
+      }
+      return res.status(404).json({ error: 'Not Found' });
+    })
+    .then(data => {
+      console.log(data)
+      if (data.empty) return db
+        .collection('likes')
+        .add({
+          wordId: req.params.wordId,
+          userName: req.user.name
+        })
+        .then(() => {
+          wordData.likeCount++;
+          return wordDocument.update({ likeCount: wordData.likeCount });
+        })
+        .then(() => res.json(wordData))
+        .catch(err => res.status(500).json({ error: err.code }));
+      return res.status(400).json({ error: 'Already liked'});
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code })
+    }
+      );
 }
 
 exports.unlikeWord = (req, res) => {
-  const likeDocument = db
-    .collection('likes')
-    .where('userName', '==', req.user.name)
-    .where('wordId', '==', req.params.wordId)
-    .limit(1);
+  const likeDocument = findLikeDocument(req);
+  const wordDocument = findWordDocument(req);
+  let wordData = {};
 
-    const wordDocument = db.doc(`/words/${req.params.wordId}`)
+  wordDocument
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        wordData = doc.data();
+        wordData.wordId = doc.id;
+        return likeDocument.get();
+      }
+      return res.status(404).json({ error: 'Not Found' });
+    })
+    .then(data => {
+      if (!data.empty) return db
+        .doc(`likes/${data.docs[0].id}`)
+        .delete()
+        .then(() => {
+          wordData.likeCount--;
+          return wordDocument.update({ likeCount: wordData.likeCount });
+        })
+        .then(() => res.json(wordData))
+        .catch(err => res.status(500).json({ error: err.code }));
 
-    let wordData = {};
-
-    wordDocument
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          wordData = doc.data();
-          wordData.wordId = doc.id;
-          return likeDocument.get()
-        }
-        return res.status(404).json({ error: 'Not Found' })
-      })
-      .then(data => {
-        if (!data.empty) return db
-          .doc(`likes/${data.docs[0].id}`)
-          .delete()
-          .then(() => {
-            wordData.likeCount--;
-            return wordDocument.update({ likeCount: wordData.likeCount });
-          })
-          .then(() => res.json(wordData))
-          .catch(err => res.status(500).json({ error: err.code }));
-
-        return res.status(400).json({ error: 'Has not been liked'});
-      })
-      .catch(err => res.status(500).json({ error: err.code }));
+      return res.status(400).json({ error: 'Has not been liked'});
+    })
+    .catch(err => res.status(500).json({ error: err.code }));
 }
---
+++ functions/utils/dbDocument.js
@@ -0,0 +1,11 @@
+const { db } = require('./admin'); 
+
+exports.findLikeDocument = (req) => {
+  return db
+    .collection('likes')
+    .where('userName', '==', req.user.name)
+    .where('wordId', '==', req.params.wordId)
+    .limit(1);
+}
+
+exports.findWordDocument = (req) => db.doc(`/words/${req.params.wordId}`);
