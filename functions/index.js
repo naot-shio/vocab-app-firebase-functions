ec970530090a19d53ec410b1e1a99a47cb578f0c
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-06-17 17:58:45 +0900

    Install cors and configure it

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
 const functions = require('firebase-functions');
-
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 const FBAuth = require('./utils/fbAuth');
-
 const { db } = require('./utils/admin');
 
 const { 
